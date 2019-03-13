39edbd8d0ad464e775c342b2ebf038791a9d2775
Gleyson Carvalho <gleyson_10@hotmail.com>
2019-03-13 00:27:26 -0300

    primeiro commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        //check existing email
+        $user = User::where('email',$request->email)->first();
+        if ($user){
+            return response()->json(['error'=>'Email already registered'], 400);
+
+        }
+
+        //registering user
+        $user = User::create([
+            'name'  => $request->name,
+            'email'    => $request->email,
+            'password' => $request->password,
+        ]);
+
+        $token = auth()->login($user);
+
+        return $this->respondWithToken($token);
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    protected function respondWithToken($token)
+    {
+        $user = auth()->user();
+        return response()->json([
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
